14f50c16166fa8ec0a04528c7e21c1587592a08f
Adrian Leon <220037530@upq.edu.mx>
2023-05-06 18:38:52 -0600

    first commit

---
+++ Launcher-eMQTT-DataSynth.sh
@@ -0,0 +1,11 @@
+export EMQTT_DBNAME=datasynth
+export EMQTT_HOSTNAME=localhost
+export EMQTT_HOSTNAME_MQTT=localhost
+export EMQTT_PASSWORD=
+export EMQTT_PORT=3306
+export EMQTT_USERNAME=root
+export MQTT_PASSWORD=abcd1234
+export MQTT_USERNAME=Main
+
+
+java -jar eMQTT-Datasynth.jar -Dlog4j.configurationFile=/eMQTT/eMQTT-DataSynth/log4j2.xml
